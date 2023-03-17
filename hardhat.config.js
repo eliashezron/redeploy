7dcfd07f54eee7dcbc88a16cf083677870f52afa
Elias Hezron <eliashezron@Eliass-MacBook-Pro.local>
2023-03-17 10:53:49 +0300

    ready for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,6 @@ require("@nomiclabs/hardhat-ethers")
 require("@openzeppelin/hardhat-upgrades")
 require("@nomiclabs/hardhat-etherscan")
 require("ethers")
-// require("@nomiclabs/hardhat-web3")
-// require("hardhat-deploy")
-// FEANRUXR1JIHDQ76YXIDFV5PE27EIJRNPA
-// 6UKT81CI5KJVJFJCXKZV7RIXKYNJM27TCB
 module.exports = {
   defaultNetwork: "hardhat",
   etherscan: {
@@ -24,21 +20,13 @@ module.exports = {
     },
     hardhat: {},
     mainnet: {
-      url: "https://speedy-nodes-nyc.moralis.io/27da94136479eb96f635fbf9/eth/mainnet",
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
       saveDeployments: true,
       chainId: 1,
       timeoutBlocks: 2000,
       gas: 2100000,
       gasPrice: 30000000000,
-    },
-    rinkeby: {
-      url: "https://speedy-nodes-nyc.moralis.io/0f495cca072b76ea382641a6/eth/rinkeby",
-      accounts: [process.env.PRIVATE_KEY],
-      saveDeployments: true,
-      chainId: 4,
-      gas: 2100000,
-      gasPrice: 8000000000,
     },
     bscTestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
@@ -71,5 +59,3 @@ module.exports = {
     timeout: 20000,
   },
 }
-// 0x068726825cadC89558D8f9173b4782E6A170cEcd
-// 0x9ce8e27ee2fbd2de6b15d631ff0f6ba90f0fbf16
